7b800828209573067487292da69ea0dc1cefc71a
yqcznb <485737827@qq.com>
2019-07-02 15:02:08 +0800

    下这个

---
+++ nb/src/components/login/login.vue
@@ -0,0 +1,125 @@
+<template>
+    <div id="login">
+        <div class="login_continue">
+            <span>趣跑登录</span> 
+            <router-link to=""><span class="continue">跳过</span></router-link>
+        </div>
+        <form>
+            <input tyoe=“text” placeholder="用户名/邮箱号/手机号">
+            <input type="password" placeholder="登录密码">
+            <button>登录</button>
+            <div class="jump_bar">
+                <router-link to="register"><span class="jump register">手机快速注册</span></router-link>
+                <router-link to="forget"><span class="jump forget">忘记密码</span></router-link>
+            </div>
+            
+        </form>
+        <span class="other_login">第三方登录</span> <hr>
+        <div class="login_bar">
+            <img class="login_choose login_wechat" src="../../assets/img/wechat.png">
+            <img class="login_choose login_QQ" src="../../assets/img/QQ.png">
+            <img class="login_choose login_sina" src="../../assets/img/sina.png">
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'login',
+    data() {
+        return {
+            msg: 'Here is login component'
+        }
+    }
+}
+</script>
+<style scoped>
+    a {
+        text-decoration: none;
+    }
+    .jump_bar {
+        margin-top: 12px;
+        display: flex;
+    }
+    .jump {
+        font-size: 12px;
+        color: #8a8c8e;
+    }
+    .register {
+        position: fixed;
+        left: 0;
+        margin-left: 1.5em;
+    }
+    .forget {
+        position: fixed;
+        right: 0;
+        margin-right: 1.5em;
+    }
+    .login_continue {
+        width: 100%;
+        height: 44px;
+        position: fixed;
+        top: 0;
+        text-align: center;
+        background-color: #f2f2f2;
+        line-height: 44px;
+    }
+    .continue {
+        color: #2c3e50;
+        font-size: 15px;
+        position: fixed;
+        right: 0;
+        margin-right: 1.1em;
+    }
+    input {
+        width: 90%;
+        height: 44px;
+        border: 0;
+        background-color: #f2f2f2;
+        margin: 10px auto;
+        display: block;
+        font-size: 13px;
+        text-indent: 1em;
+    }
+    button {
+        width: 90%;
+        height: 44px;
+        background-color: #d5a269;
+        border: 0;
+    }
+    .other_login {
+        width: 6em;
+        text-align: center;
+        position: fixed;
+        left: 0;
+        right: 0;
+        margin: 0 auto;
+        bottom: 99px;
+        font-size: 13px;
+        color: #a9a9a9;
+        background-color: white;
+        z-index: 10;
+    }
+    hr {
+        width: 80%;
+        position: fixed;
+        left: 0;
+        right: 0;
+        margin: 0 auto;
+        bottom: 105px;
+    }
+    .login_bar {
+        width: 100%;
+        position: fixed;
+        bottom: 30px;
+    }
+    .login_choose {
+        width: 30px;
+        height: 30px;  
+    }
+    .login_bar {
+        display: flex;
+        /* flex-direction: row; */
+        justify-content: space-around;
+    }
+</style>
+
