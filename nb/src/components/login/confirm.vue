06ea2d65ee38fea22c0600625420fb40f4a878b6
yqcznb <485737827@qq.com>
2019-08-24 21:34:42 +0800

    11

--- nb/src/components/login/confirm.vue
+++ nb/src/components/login/confirm.vue
@@ -1,20 +1,19 @@
 <template>
     <div class="confirm">
         <mt-header title="身份认证">
-        <router-link to="/footer/my" slot="left">
+        <router-link to="/footer/index" slot="left">
             <mt-button icon="back" @click="back">返回</mt-button>
         </router-link>
         <mt-button slot="right">修改信息</mt-button>
         </mt-header>
-        <mt-field label="学校" placeholder="青岛工学院" v-model="school"></mt-field>
-        <mt-field label="院系" placeholder="信息工程学院" type="email" v-model="yuanxi"></mt-field>
-        <mt-field label="专业" placeholder="软件工程" type="password" v-modal="zhuanye"></mt-field>
-        <mt-field label="姓名" placeholder="黄金文" type="tel" v-model="name"></mt-field>
-        <mt-field label="学号" placeholder="201702505253" type="url" v-model="stunumber"></mt-field>
-        <mt-field label="性别" placeholder="女" type="number" v-model="xy"></mt-field>
-        <mt-field label="入学年份" placeholder="2017" v-model="schage"></mt-field>
-        <mt-button disabled>已认证</mt-button>
-        
+        <mt-field label="学校" v-model="yhxh" :disabled="disabled"></mt-field>
+        <mt-field label="院系"   v-model="yhxy" :disabled="disabled"></mt-field>
+        <mt-field label="专业"   v-modal="yhzy" :disabled="disabled"></mt-field>
+        <mt-field label="姓名"  v-model="name" :disabled="disabled"></mt-field>
+        <mt-field label="学号"  v-model="stunumber" :disabled="disabled"></mt-field>
+        <mt-field label="性别"  v-model="xy" :disabled="disabled"></mt-field>
+        <mt-field label="入学年份" v-model="schage" :disabled="disabled"></mt-field>
+        <mt-button type="primary" :disabled="disabled" @click="renzhen">认证</mt-button>     
 </div>
 </template>
 <script>
@@ -22,17 +21,48 @@ export default {
     name: 'confirm',
     data() {
         return {
-       
+            disabled:false,
         }
+    },
+    created(){
+         axios.get('http://no37.store:8080/AK/SelectXsID',{
+    params: {
+        yhid:localStorage.getItem("yhid"),     
+    }
+}).then(response=>{
+          if(response.data.yhxx!=""&&response.data.yhxx!=null&&response.data.yhxx!=undefined){
+              this.disabled = true
+          }
+        
+      })      //获取失败
+      .catch(error=>{
+          console.log(error);
+          alert('网络错误，不能访问');
+      })
     },
     methods:
     {
        
         back(){
                 this.$router.push({
-                    path: '/footer/my'
+                    path: '/footer/index'
                 });
                  window.location.reload();
+        },
+        renzhen(){
+               axios.get('http://no37.store:8080/AK/xsID',{
+        params: {
+            yhid:localStorage.getItem("yhid"),    
+            
+        }
+    }).then(response=>{
+           
+            
+        })      //获取失败
+      .catch(error=>{
+          console.log(error);
+          alert('网络错误，不能访问');
+      })
         }
     }
 }
