9372d8871e3278d6ae00c02edacd39dc5b9e7b87
F-nothing <babalalw@gmail.com>
2019-08-27 20:21:35 +0800

    axiba08272021--2

--- nb/src/components/my/settings/pinform/pinform.vue
+++ nb/src/components/my/settings/pinform/pinform.vue
@@ -8,17 +8,25 @@
         <div id="photo_name">
             <router-link to="">
                 <div id="photo">
-                    <span class="left-text head_title">头像</span>
-                    <img :src='headportrait' class="headportrait">
+                    <span class="head_left head_title">头像</span>
+                    <span class="head_right">
+                        <img :src='headportrait' class="headportrait"><i class="iconfont iconfanhui iconfont-right"></i>
+                    </span>
                 </div>
             </router-link>
             <hr>
             <router-link to="">
                 <div id="name">
-                    <span><span class="name_title">昵称</span><span class="rightname" v-text="user_name"></span></span><i class="iconfont iconfanhui iconfont-right"></i>
+                    <span class="name_left name_title">昵称</span>
+                    <span class="name_right">
+                        <span v-text="user_name" class="user_name"></span><i class="iconfont iconfanhui iconfont-right"></i>
+                    </span>
                 </div>
             </router-link>
         </div>
+        
+        <input class="file" name="file" type="file" accept="image/png,image/gif,image/jpeg" @change="update"/>
+
     </div>
 </template>
 <script>
@@ -29,6 +37,7 @@ export default {
         return{
             user_name: '',
             headportrait: '',
+            file: {},
         }
     },
     created() {
@@ -46,11 +55,28 @@ export default {
             .catch(error=>{
                 alert('网络错误，不能访问');
             })
+        
     },
     methods: {
         routerRefresh() {
             window.location.reload();
         },
+        update(e){
+            let file = e.target.files[0];
+            let param = new FormData(); //创建form对象
+            param.append('file',file);//通过append向form对象添加数据
+            console.log(param.get('file')); //FormData私有类对象，访问不到，可以通过get判断值是否传进去
+            axios.get('http://no37.store:8080/AK/AddPhoto',{
+                params: {
+                    yhid:localStorage.getItem("yhid"),file:param,
+                }})
+            .then(response=>{
+                console.log(response.data);
+            })
+            .catch(error=>{
+                console.log(error);
+            })
+        }
     },
 }
 </script>
@@ -76,7 +102,7 @@ export default {
     }
     #photo_name {
         width: 93%;
-        height: 16%;
+        height: 17%;
         border-radius: 7px;
         background-color: white;
         display: flex;
@@ -99,34 +125,46 @@ export default {
     #photo {
         display: flex;
         justify-content: space-between;
-        padding-right: 2%;
+        align-items: center;
+        /* padding-right: 13px; */
+    }
+    #name {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        /* padding-right: 13px; */
 
+    }
+    .head_right,.name_right {
+        width: 25%;
+        max-width: 90px;
+        display: inline-flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-right: 12px;
     }
     .headportrait {
         border-radius: 7px;
         position: relative;
-        width: 40px;
+        width: 55%;
+        min-width: 44px;
+        margin-right: 7%;
     }
     .iconfont-right {
         font-size: 20px;
         color: #999999;
-        margin-right: 0;
-        position: absolute;
-        right: 7%;
+        /* margin-right: 0; */
+        /* right: 7%; */
     }
     .head_title,.name_title {
         color: black;
     }
-    .left-text,.headportrait {
-        display: flex;
-        justify-content: space-around;
-    }
-    .rightname {
+    .user_name {
         font-size: 50%;
         color: #999999;
         display: inline-block;
-        float: right;
-        margin-right: 10%;
+        /* margin-right: 10%; */
     }
 </style>
 
